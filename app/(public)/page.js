8733a4437e54978da43206a70656509cf9f6b201
Safo Emmanuel <nanasafo122@gmail.com>
2025-07-22 15:38:09 +0000

    12

--- app/(public)/page.js
+++ app/(public)/page.js
@@ -84,7 +84,7 @@ export default async function Home() {
         <div className="container mx-auto px-4">
           <div className="text-center mb-12">
             <h2 className="text-3xl font-bold tracking-tight sm:text-4xl text-blue-950">
-              What's New
+              Whats New
             </h2>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
